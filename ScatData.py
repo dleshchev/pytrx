db6ebcd177a6031352dfebdf87d0255511022e5b
dleshchev <leshchev.denis@gmail.com>
2018-06-04 21:04:18 -0500

    refactored the __init__ method

--- ScatData.py
+++ ScatData.py
@@ -32,58 +32,50 @@ from matplotlib import pyplot as plt
 
 class ScatData:
     
-    def __init__(self, logFile, dataInDir, dataOutDir, maskPath, energy, 
-                 distance, pixelSize, centerX, centerY, qNormRange,
-                 toff_str = '-5us'):
+    def __init__(self, logFile, dataInDir, dataOutDir):
+#    , maskPath, energy, 
+#                 distance, pixelSize, centerX, centerY, qNormRange,
+#                 toff_str = '-5us'):
                 
         self.logFile = logFile
         self.dataInDir = dataInDir
         self.dataOutDir = dataOutDir
-        self.maskPath = maskPath        
-        
-        self.energy = energy
-        self.wavelength = 12.3984/self.energy*1e-10 # in m
-        self.distance = distance*1e-3 # in m
-        self.pixelSize = pixelSize
-        self.centerX = centerX
-        self.centerY = centerY
 
-        self.toff_str = toff_str
-        self.toff = self.time_str2num(toff_str)
-        self.qNormRange = qNormRange
+#        self.maskPath = maskPath
+#        
+#        self.energy = energy
+#        self.wavelength = 12.3984/self.energy*1e-10 # in m
+#        self.distance = distance*1e-3 # in m
+#        self.pixelSize = pixelSize
+#        self.centerX = centerX
+#        self.centerY = centerY
+#
+#        self.toff_str = toff_str
+#        self.toff = self.time_str2num(toff_str)
+#        self.qNormRange = qNormRange
+#        
+        assert Path(self.logFile).is_file(), 'log file not found'
+        assert Path(self.dataInDir).is_dir(), 'input directory not found'
+        assert Path(self.dataOutDir).is_dir(), 'output directory not found'
         
-        self.valCheck()
+        self.logData = pd.read_csv(logFile, sep = '\t', header = 18)
+        self.logData.rename(columns = {'#date time':'date time'}, inplace = True)
         
-        self.getLogData()    
-#        self.getDelaysNum()
-#        self.getTimeStamps()
-
-#        self.getDifferences()
-
+        # remove files that for whatever reason do not exist
+        idxToDel = []
+        for i, row in self.logData.iterrows():
+            if not Path(self.dataInDir + self.logData.ix[i,'file']).is_file():
+                idxToDel.append(i)
+        self.logData = self.logData.drop(idxToDel)
         
-    def valCheck(self):
-        if not Path(self.logFile).is_file():
-            print("log file does not exist")
-        if not Path(self.dataInDir).is_dir():
-            print("input directory does not exist")
-        if not Path(self.dataOutDir).is_dir():
-            print("output directory does not exist")
+        # get the number of time delays and number of files
+        print('Found {} files'.format(len(A.logData['delay'].tolist())))
+        print('Found {} time delays'.format(len(np.unique(A.logData['delay'].tolist()))))
 
 
 
     def getLogData(self):
-        self.logData = pd.read_csv(self.logFile,
-                                       sep = '\t',
-                                       header = 18)
-        self.logData.rename(columns = {'#date time':'date time'}, inplace = True)
-        # Add a column that indicates existance of the files
-        self.logData['flagExist'] = False        
-        # For those files that exist, we put True flag
-        for i, row in self.logData.iterrows():
-            if Path(self.dataInDir + self.logData.ix[i,'file']).is_file():
-                self.logData.set_value(i,'flagExist' , True)
-        
-        
+
 
     def getAIGeometry(self):
         # First, lets get the geometry parameters for integration
@@ -104,43 +96,45 @@ class ScatData:
         # Now lets get the integration going
         if not hasattr(self, 'AIGeometry'):
             self.getAIGeometry()
-            self.total = namedtuple('total','S S_raw normInt delay delay_str timeStamp timeStamp_str scanStamp')        
+            self.total = namedtuple('total','s s_raw normInt delay delay_str t t_str timeStamp timeStamp_str scanStamp')        
             nFiles = self.logData.shape[0]        
-            self.total.S = np.zeros([self.AIGeometry.nPt, nFiles])
-            self.total.S_raw = np.zeros([self.AIGeometry.nPt, nFiles])
+            self.total.s = np.zeros([self.AIGeometry.nPt, nFiles])
+            self.total.s_raw = np.zeros([self.AIGeometry.nPt, nFiles])
             self.total.normInt = np.zeros(nFiles)
             self.total.delay, self.total.delay_str = self.getDelays()
+            self.total.t = np.unique(self.total.delay)
+            self.total.t_str = np.unique(self.total.delay_str)
             self.total.timeStamp, self.total.timeStamp_str = self.getTimeStamps()
             self.total.scanStamp = [ntpath.splitext(ntpath.basename(self.logFile))[0]]*nFiles
-        
-        idxIm = 1
-        print('*** Integration ***')
-        for i,file in enumerate(self.logData['file']):
-            path = (self.dataInDir + file)
-            startReadTime = time.clock()         
-            image = fabio.open(path).data
-            readTime = time.clock() - startReadTime
             
-            startIntTime = time.clock()
-            q, self.total.S_raw[:,i] = self.AIGeometry.ai.integrate1d(
-                                    image,
-                                    self.AIGeometry.nPt,
-                                    radial_range = self.AIGeometry.radialRange,
-                                    correctSolidAngle = True,
-                                    polarization_factor = 1,
-                                    method = 'lut',
-                                    mask = self.AIGeometry.mask,
-                                    unit = "q_A^-1")
-            intTime = time.clock() - startIntTime
-            print('Integration of image', idxIm, '(of', nFiles, ') took', '%.0f' % (intTime*1e3), 'ms of which', '%.0f' % (readTime*1e3), 'ms was spent on readout')
-            idxIm += 1
-            qNormRangeMask = (q>=self.qNormRange[0]) & (q<=self.qNormRange[1])
-            self.total.normInt[i] = np.trapz(self.total.S_raw[qNormRangeMask,i], 
-                                             q[qNormRangeMask])
-            self.total.S[:,i] = self.total.S_raw[:,i]/self.total.normInt[i]
-        print('*** Integration done ***')
-        self.q = q
-        plt.plot(self.q, self.total.S)
+#        idxIm = 1
+#        print('*** Integration ***')
+#        for i,file in enumerate(self.logData['file']):
+#            path = (self.dataInDir + file)
+#            startReadTime = time.clock()         
+#            image = fabio.open(path).data
+#            readTime = time.clock() - startReadTime
+#            
+#            startIntTime = time.clock()
+#            q, self.total.s_raw[:,i] = self.AIGeometry.ai.integrate1d(
+#                                    image,
+#                                    self.AIGeometry.nPt,
+#                                    radial_range = self.AIGeometry.radialRange,
+#                                    correctSolidAngle = True,
+#                                    polarization_factor = 1,
+#                                    method = 'lut',
+#                                    mask = self.AIGeometry.mask,
+#                                    unit = "q_A^-1")
+#            intTime = time.clock() - startIntTime
+#            print('Integration of image', idxIm, '(of', nFiles, ') took', '%.0f' % (intTime*1e3), 'ms of which', '%.0f' % (readTime*1e3), 'ms was spent on readout')
+#            idxIm += 1
+#            qNormRangeMask = (q>=self.qNormRange[0]) & (q<=self.qNormRange[1])
+#            self.total.normInt[i] = np.trapz(self.total.S_raw[qNormRangeMask,i], 
+#                                             q[qNormRangeMask])
+#            self.total.s[:,i] = self.total.S_raw[:,i]/self.total.normInt[i]
+#        print('*** Integration done ***')
+#        self.q = q
+#        plt.plot(self.q, self.total.S)
 
 
         
@@ -149,7 +143,9 @@ class ScatData:
         delay = []
         for t_str in delay_str:
             delay.append(self.time_str2num(t_str))            
+        delay_str = np.array(delay_str)
         delay = np.array(delay)
+        
         return delay, delay_str
 
 
@@ -158,14 +154,15 @@ class ScatData:
         try:
             t = float(t_str)
         except ValueError:
+            t_number = float(t_str[0:-2])
             if 'ps' in t_str:
-                t = float(t_str[0:-2])*1e-12
+                t = t_number*1e-12
             elif 'ns' in t_str:
-                t = float(t_str[0:-2])*1e-9
+                t = t_number*1e-9
             elif 'us' in t_str:
-                t = float(t_str[0:-2])*1e-6
+                t = t_number*1e-6
             elif 'ms' in t_str:
-                t = float(t_str[0:-2])*1e-3
+                t = t_number*1e-3
         return t
 
 
@@ -181,14 +178,35 @@ class ScatData:
     
     def getDifferences(self):
         
-        idx_on = np.abs(self.delays-self.toff)>1e-12
-        idx_off = np.abs(self.delays-self.toff)<1e-12
-        S_on = self.S[idx_on]
-        S_off = self.S[idx_off]
-        delays_on = self.delays[idx_on]
+        self.diff = namedtuple('ds', 'ds delay t timeStamp')
+        self.diff.ds = []
+        self.diff.delay = []
+        self.diff.t = self.total.t
+        self.diff.t_str = self.total.t_str
+        self.diff.timeStamp = []
+        
+        delaySelIDRef = self.total.delay_str == self.toff_str
+        s_ref = self.total.s[:, delaySelIDRef]
+        timeStamp_ref = self.total.timeStamp[delaySelIDRef]
+        timeStampThresh = np.median(timeStamp_ref)*1.1
+        
+        for specificDelay in self.diff.t:
+            if self.diff.t_str == self.toff_str:
+                pass
+                
+            else:
+                delaySelID = self.total.delay == specificDelay
+                s_loc = self.total.s[delaySelID]
+                timeStamp_loc = self.total.timeStamp[delaySelID]
+                timeStampDif_loc = np.abs(timeStamp_loc[newaxis].T - timeStamp_ref)
+                timeStampDif_ID = timeStampDif_loc<=timeStampThresh
+                s_ref_tbs = np.mean(s_ref[:,timeStampDif_ID], axis = 1)
+                ds_dummy = s_loc - s_ref_tbs
+                
+        
+        
+        
 #        delays_off = self.delays[idx_off]   
-        timeStamps_on = self.timeStamps[idx_on]
-        timeStamps_off = self.timeStamps[idx_off]
         
         self.dS = np.zeros([np.shape(self.q)[0], np.shape(delays_on)[0]])
         
@@ -202,12 +220,12 @@ class ScatData:
 # check the data
 A = ScatData(logFile = '/media/denis/Data/work/Experiments/2017/Ubiquitin/10.log',
              dataInDir = '/media/denis/Data/work/Experiments/2017/Ubiquitin/',
-             dataOutDir = '/media/denis/Data/work/Experiments/2017/Ubiquitin/',
-             maskPath = '/media/denis/Data/work/Experiments/2017/Ubiquitin/mask_aug2017.tif',
-             energy = 11.63,
-             distance = 362,
-             pixelSize = 82e-6,
-             centerX = 1990,
-             centerY = 1967,
-             qNormRange = [2,3],
-             toff_str = '-5us')
+             dataOutDir = '/media/denis/Data/work/Experiments/2017/Ubiquitin/')
+#             maskPath = '/media/denis/Data/work/Experiments/2017/Ubiquitin/mask_aug2017.tif',
+#             energy = 11.63,
+#             distance = 362,
+#             pixelSize = 82e-6,
+#             centerX = 1990,
+#             centerY = 1967,
+#             qNormRange = [2,3],
+#             toff_str = '-5us')
