5c985ccd84e212db6f08818f3051a5f4a0f0aba0
Denis Leshchev <leshchev.denis@gmail.com>
2020-04-06 23:15:13 -0400

    all rmc-related stuff now is in a separate file

--- pytrx/utils.py
+++ pytrx/utils.py
@@ -117,3 +117,19 @@ def cov2corr(C):
     d = np.diag(1/np.sqrt(np.diag(C)))
     return d @ C @ d
 
+
+def z_num2str(z):
+    return ElementString()[z - 1]
+
+
+def z_str2num(z):
+    for i, el in enumerate(ElementString()):
+        if el == z:
+            return i + 1
+
+
+def ElementString():
+    ElementString = 'H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og'
+    return ElementString.split()
+
+
