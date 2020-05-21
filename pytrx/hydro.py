1e83726b73e9c4f7f2003e67a2559776a30fca79
Denis Leshchev <leshchev.denis@gmail.com>
2020-05-21 15:14:12 -0400

    added solvent data to hydro.py

--- pytrx/hydro.py
+++ pytrx/hydro.py
@@ -13,16 +13,32 @@ def add_solvent(d, keys, hp_obj):
         d[key] = hp_obj
 
 
+data = {}
+
+
 acetonitrile = HydroProperties(density=786,                     # kg/m3
                                molar_mass=41.05e-3,             # kg/mol
                                )
-
-data = {}
-
 acetonitrile_keys = ['acetonitrile', 'Acetonitrile', 'acn', 'MeCN', 'ACN', 'CH3CN', 'ch3cn']
 add_solvent(data, acetonitrile_keys, acetonitrile)
 
 
+
+cyclohexane = HydroProperties(density=779,                     # kg/m3
+                               molar_mass=84.16e-3,             # kg/mol
+                               )
 cyclohexane_keys = ['cyclohexane', 'c6h12', 'C6H12']
+add_solvent(data, cyclohexane, cyclohexane_keys)
+
+
+tetrahydrofuran = HydroProperties(density=889,                     # kg/m3
+                               molar_mass=72.11e-3,             # kg/mol
+                               )
 tetrahydrofuran_keys = ['thf', 'THF', 'Tetrahydrofuran', 'tetrahydrofuran']
+add_solvent(data, tetrahydrofuran, tetrahydrofuran_keys)
+
+water = HydroProperties(density=1000,                     # kg/m3
+                               molar_mass=18.01528e-3,             # kg/mol
+                               )
 water_keys = ['Water', 'water', 'h2o', 'H2O']
+add_solvent(data, water, water_keys)
