cdeeafd8c31237ef9682a82a8387a676f1cc3272
darrenjhsu <darrenhsu2015@u.northwestern.edu>
2020-06-10 18:08:32 -0500

    Used issubclass in scatsim.py to check input

--- pytrx/scatsim.py
+++ pytrx/scatsim.py
@@ -36,12 +36,17 @@ class Molecule:
 
         if associated_transformation is None:
             self._associated_transformation = None
-        elif type(associated_transformation) != list:
+        elif type(associated_transformation) == list:
+            print("associated_transformation is a list. Examining elements...")
+            for t in associated_transformation:
+                assert issubclass(type(t), Transformation), 'List element is not a Transformation class'
+            self._associated_transformation = associated_transformation
+        elif issubclass(type(associated_transformation), Transformation):
             self._associated_transformation = [associated_transformation]
             # This is quite dangerous, maybe need to assert it's a Transformation base class
             # Will be implemented in the future
         else:
-            self._associated_transformation = associated_transformation
+            raise TypeError('Supplied transformations must be None, a transformation class, or a list of it')
 
         if self._associated_transformation is not None:
             for transform in self._associated_transformation:
