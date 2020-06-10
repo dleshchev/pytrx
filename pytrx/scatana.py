Changed transformation.py to abstract methods, fixed rotation, added atomcolor and vdWradius for visualization(todo)

pytrx/scatana.py
@@ -1,5 +1,4 @@
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 from pytrx.scatdata import ScatData
@@ -40,7 +39,7 @@ class SmallMoleculeProject:
             input_data - .h5 file created using ScatData.save method
             **kwargs - any metadata you like, e.g. concnetration=10, solvent='water', etc
         '''
-        #print(type(input_data), type(input_data) == ScatData)
+        # print(type(input_data), type(input_data) == ScatData)
         if type(input_data) == str:
             self.data = ScatData(input_data, smallLoad=True)
         elif type(input_data) == ScatData:
@@ -84,7 +83,7 @@ class SmallMoleculeProject:
         if plotting:
             plt.figure(fig)
             plt.clf()
-            plt.plot(q, s_off*scale, 'k-', label=('data (' + str(self.data.diff.toff_str) + ')'))
+            plt.plot(q, s_off *scale, 'k-', label=('data (' + str(self.data.diff.toff_str) + ')'))
             plt.plot(q, s_th, 'r--', label='solvent (gas)')
             plt.xlabel('q, 1/A')
             plt.ylabel('S(q), e.u.')
@@ -197,10 +196,10 @@ class Solute:
             return input
 
 
-    def signal(self, q, pars=None):
-        # pars is a list for BOTH excited state (first) and ground state (second)
-        if pars is None:
-            pars_es, pars_gs = None, None
+    # def signal(self, q, pars=None):
+    #     # pars is a list for BOTH excited state (first) and ground state (second)
+    #    if pars is None:
+    #        pars_es, pars_gs = None, None
 
     def s(self, q, pars=None, target='mol_es'):
         '''
@@ -218,30 +217,29 @@ class Solute:
                 return scatsim.Debye(q, self.mol_gs)
             else:
                 return np.zeros(q.shape)
-
         else:
             print("No signal is calculated as no target is specified. None returned.")
 
-
-    def ds(self, q, pars):
+    def ds(self, q, pars=None):
         '''
         Originally just 'signal' but changed to 'ds' as this is calculating difference signal
         '''
         # self.mol_es.move(*x) - consider this
-        pars_es, pars_gs = deal_pars(pars, self.n_par_total)
+        pars_es, pars_gs = deal_pars(pars, self.mol_es.n_par)
+        print(f'ES parameters: {pars_es}, GS parameters: {pars_gs}')
         return self.s(q, pars=pars_es, target='mol_es') - self.s(q, pars=pars_gs, target='mol_gs')
 
     def list_pars(self):
         # Because we pass to signal() a list of parameters which is not intuitive
-        print(f'List paramters: \n'
+        print(f'Listing parameters: \n'
               f'There are {self.n_par_total} parameters to be passed '
-              f'to the pars argument as a list for signal method\n')
+              f'to the pars argument as a list for ds method\n')
         for i in np.arange(self.mol_es.n_par):
-            print(f'Parameter {i+1}: ES, {type(self.mol_es._associated_transformation[i])}')
+            print(f'Parameter { i +1}: ES, {type(self.mol_es._associated_transformation[i])}')
             self.mol_es._associated_transformation[i].describe()
             print("")
         for i in np.arange(self.mol_gs.n_par):
-            print(f'Parameter {i+1+self.mol_es.n_par}: ES, {type(self.mol_gs._associated_transformation[i])}')
+            print(f'Parameter { i + 1 +self.mol_es.n_par}: ES, {type(self.mol_gs._associated_transformation[i])}')
             self.mol_gs._associated_transformation[i].describe()
             print("")
         # if target == 'mol_es':
@@ -252,14 +250,14 @@ class Solute:
         #     print("No signal is calculated as no target is specified. None returned.")
 
 
-    def ds(self, q, pars):
-        '''
-        Originally just 'signal' but changed to 'ds' as this is calculating difference signal
-        '''
-        # self.mol_es.move(*x) - consider this
-        pars_es, pars_gs = deal_pars(pars, self.mol_es.n_par)
-        print(pars_es, pars_gs)
-        return self.s(q, pars=pars_es, target='mol_es') - self.s(q, pars=pars_gs, target='mol_gs')
+    # def ds(self, q, pars):
+    #     '''
+    #     Originally just 'signal' but changed to 'ds' as this is calculating difference signal
+    #     '''
+    #     # self.mol_es.move(*x) - consider this
+    #     pars_es, pars_gs = deal_pars(pars, self.mol_es.n_par)
+    #     print(pars_es, pars_gs)
+    #     return self.s(q, pars=pars_es, target='mol_es') - self.s(q, pars=pars_gs, target='mol_gs')
 
 
             # scatsim.Debye(q, self.mol_es) - scatsim.Debye(q, self.mol_gs)
@@ -332,12 +330,6 @@ def deal_pars(pars, n):
         pars_es, pars_gs = None, None
     else:
         pars_es, pars_gs = pars[:n], pars[n:]
-
-    if len(pars_es) == 0: pars_es = None
-    if len(pars_gs) == 0: pars_gs = None
+        if len(pars_es) == 0: pars_es = None
+        if len(pars_gs) == 0: pars_gs = None
     return pars_es, pars_gs
-
-
-
-
-

pytrx/transformation.py
@@ -14,7 +14,7 @@ from pytrx.utils import AtomicMass
 from abc import (ABC as _ABC, abstractmethod as _abstractmethod)
 
 
-class Transformtaion(_ABC):
+class Transformation(_ABC):
     ''' Abstract class for transformations
 
     '''
@@ -30,14 +30,16 @@ class Transformtaion(_ABC):
     def describe(self, mol):
         '''transformation description '''
 
+
     @_abstractmethod
-    def transform(self, xyz, amplitude0=0):
-        ''' takes xyz and returns an updated one'''
+    def transform(self, xyz, amplitude=None):
+        pass
 
 
-class Transformation_move_vector:
+class Transformation_move_vector(Transformation):
     # Move a group of atoms along a vector (normalized)
     def __init__(self, group1, vector, amplitude0=0):
+        super().__init__()
         self.group1 = np.array(group1)
         self.vector = np.array(vector)
         self.unit_vector = np.array(vector) / np.linalg.norm(vector)
@@ -46,7 +48,7 @@ class Transformation_move_vector:
     def prepare(self, mol):
         assert (np.max(self.group1) <= len(mol.xyz)), \
             "Index out of bound: largest index of group 1 > length of supplied molecule"
-        return self
+        # return self
 
     def describe(self):
         print("  Moving group1 along a predefined vector. Both groups move.")
@@ -54,15 +56,15 @@ class Transformation_move_vector:
         print(f'    Vector : {self.unit_vector}')
 
     def transform(self, xyz, amplitude=None):
-
         if amplitude is None:
             amplitude = self.amplitude0
         xyz[self.group1] += self.unit_vector * amplitude
         return xyz
 
 
-class Transformation_vibration:
+class Transformation_vibration(Transformation):
     def __init__(self, dxyz, amplitude0=0):
+        super().__init__()
         self.dxyz = dxyz
         self.amplitude0 = amplitude0
 
@@ -80,14 +82,12 @@ class Transformation_vibration:
         return (xyz + self.dxyz * amplitude)
 
 
-
-
-
-class Transformation_distance:
+class Transformation_distance(Transformation):
     # Move two groups of atoms closer/further in distance, using simple mean of coordinates as
     # reference centers for each group.
     # Vector is from group1 to group2. Negative amplitude is shrinking.
     def __init__(self, group1, group2, amplitude0=0):
+        super().__init__()
         assert (len(group1) > 0) and (len(group2) > 0), 'Cannot operate on empty set'
         self.group1 = np.array(group1)
         self.group2 = np.array(group2)
@@ -101,7 +101,7 @@ class Transformation_distance:
         self.group1_mean = np.mean(mol.xyz[self.group1], 0)
         self.group2_mean = np.mean(mol.xyz[self.group2], 0)
         self.unit_vec = (self.group2_mean - self.group1_mean) / np.linalg.norm(self.group2_mean - self.group1_mean)
-        return self
+        # return self
 
     def describe(self):
         print(f'  Increasing / decreasing distance between group1 and group2 using '
@@ -111,7 +111,6 @@ class Transformation_distance:
         print(f'    Group 2: {self.group2}')
 
     def transform(self, xyz, amplitude=None):
-
         if amplitude is None:
             amplitude = self.amplitude0
 
@@ -121,12 +120,13 @@ class Transformation_distance:
         return xyz
 
 
-class Transformation_distance_1side:
+class Transformation_distance_1side(Transformation):
     # Move GROUP 2 toward/away from GROUP 1 in distance, using simple mean of coordinates as
     # reference centers for each group.
     # Vector is from group1 to group2. Negative amplitude is shrinking.
     # GROUP 1 is fixed.
     def __init__(self, group1, group2, amplitude0=0):
+        super().__init__()
         assert (len(group1) > 0) and (len(group2) > 0), 'Cannot operate on empty set'
         self.group1 = np.array(group1)
         self.group2 = np.array(group2)
@@ -140,11 +140,11 @@ class Transformation_distance_1side:
         self.group1_mean = np.mean(mol.xyz[self.group1], 0)
         self.group2_mean = np.mean(mol.xyz[self.group2], 0)
         self.unit_vec = (self.group2_mean - self.group1_mean) / np.linalg.norm(self.group2_mean - self.group1_mean)
-        return self
+        # return self
 
     def describe(self):
         print(f'  Increasing / decreasing distance between group1 and group2 using '
-              f'simple mean of coordinates as centers.\n' 
+              f'simple mean of coordinates as centers.\n'
               f'  Only group 2 moves.')
         print(f'    Group 1: {self.group1}')
         print(f'    Group 2: {self.group2}')
@@ -157,10 +157,12 @@ class Transformation_distance_1side:
 
         return xyz
 
-class Transformation_distanceCOM:
+
+class Transformation_distanceCOM(Transformation):
     # Move two group of atoms closer/further in distance, using center of mass as ref centers for each group
     # Vector is from group1 to group2. Negative amplitude is shrinking.
     def __init__(self, group1, group2, amplitude0=0):
+        super().__init__()
         assert (len(group1) > 0) and (len(group2) > 0), 'Cannot operate on empty set'
         self.group1 = np.array(group1)
         self.group2 = np.array(group2)
@@ -179,7 +181,7 @@ class Transformation_distanceCOM:
                                  1) / np.sum(AtomicMass()[mol.Z_num[self.group2] - 1])
         self.unit_vec = (self.group2_COM - self.group1_COM) / np.linalg.norm(self.group2_COM - self.group1_COM)
         self.unit_vec = self.unit_vec.T
-        return self
+        # return self
 
     def describe(self):
         print(f'  Increasing / decreasing distance between group1 and group2 using centers of masses as centers.\n'
@@ -188,7 +190,6 @@ class Transformation_distanceCOM:
         print(f'    Group 2: {self.group2}')
 
     def transform(self, xyz, amplitude=None):
-
         if amplitude is None:
             amplitude = self.amplitude0
 
@@ -198,11 +199,12 @@ class Transformation_distanceCOM:
         return xyz
 
 
-class Transformation_distanceCOM_1side:
+class Transformation_distanceCOM_1side(Transformation):
     # Move GROUP 2 toward/away from GROUP 1 in distance, using center of mass as ref centers for each group
     # Vector is from group1 to group2. Negative amplitude is shrinking.
     # GROUP 1 is fixed.
     def __init__(self, group1, group2, amplitude0=0):
+        super().__init__()
         assert (len(group1) > 0) and (len(group2) > 0), 'Cannot operate on empty set'
         self.group1 = np.array(group1)
         self.group2 = np.array(group2)
@@ -219,7 +221,7 @@ class Transformation_distanceCOM_1side:
                                  1) / np.sum(AtomicMass()[mol.Z_num[self.group2] - 1])
         self.unit_vec = (self.group2_COM - self.group1_COM) / np.linalg.norm(self.group2_COM - self.group1_COM)
         self.unit_vec = self.unit_vec.T
-        return self
+        # return self
 
     def describe(self):
         print(f'  Increasing / decreasing distance between group1 and group2 using centers of masses as centers.\n'
@@ -228,7 +230,6 @@ class Transformation_distanceCOM_1side:
         print(f'    Group 2: {self.group2}')
 
     def transform(self, xyz, amplitude=None):
-
         if amplitude is None:
             amplitude = self.amplitude0
 
@@ -237,7 +238,7 @@ class Transformation_distanceCOM_1side:
         return xyz
 
 
-class Transformation_rotation:
+class Transformation_rotation(Transformation):
     def __init__(self, group1, axis_groups, amplitude=0):
         # A, B, and C can be group of atoms.
         # Centers will be the mean of their coordinates.
@@ -246,6 +247,7 @@ class Transformation_rotation:
         # the cross vector of AB and BC as axis.
         # Amplitude is in degrees
         # Rotation is counterclockwise for an observer to whom the axis vector is pointing (right hand rule)
+        super().__init__()
         assert (len(group1) > 0) and (len(axis_groups) > 0), 'Cannot operate on empty set'
         assert (len(axis_groups) == 2) or (len(axis_groups) == 3), 'Axis must be defined with 2 or 3 groups'
         for i in np.arange(len(axis_groups)):
@@ -265,13 +267,13 @@ class Transformation_rotation:
         if len(self.axis_groups) == 3:
             self.C_mean = np.mean(mol.xyz[self.axis_groups[2]], 0)
 
-        if len(self.axis_groups) == 2: # Then use AB as vector
+        if len(self.axis_groups) == 2:  # Then use AB as vector
             self.axis = self.B_mean - self.A_mean
 
-        if len(self.axis_groups) == 3: # Use cross product of AB and BC as vector
+        if len(self.axis_groups) == 3:  # Use cross product of AB and BC as vector
             self.axis = np.cross(self.B_mean - self.A_mean, self.C_mean - self.B_mean)
 
-        return self
+        # return self
 
     def describe(self):
         if len(self.axis_groups) == 2:
@@ -289,14 +291,18 @@ class Transformation_rotation:
             print(f'    Axis_group 3: {self.axis_groups[2]}')
 
     def transform(self, xyz, amplitude=None):
-            if amplitude is None:
-                amplitude = self.amplitude0
+        if amplitude is None:
+            amplitude = self.amplitude0
+        # Shift reference frame, rotate, then shift back
+        if len(self.axis_groups) == 2:
+            xyz[self.group1] = rotation3D((xyz[self.group1] - self.A_mean).T, self.axis, amplitude).T + self.A_mean
+        if len(self.axis_groups) == 3:
+            xyz[self.group1] = rotation3D((xyz[self.group1] - self.A_mean).T, self.axis, amplitude).T + self.B_mean
 
-            xyz[self.group1] = rotation3D(xyz[self.group1].T, self.axis, amplitude).T
+        return xyz
 
-            return xyz
 
-class Transformation_rotationCOM:
+class Transformation_rotationCOM(Transformation):
     def __init__(self, group1, axis_groups, amplitude=0):
         # A, B, and C can be group of atoms.
         # Centers will be the mean of their coordinates.
@@ -305,6 +311,7 @@ class Transformation_rotationCOM:
         # the cross vector of AB and BC as axis.
         # Amplitude is in degrees
         # Rotation is counterclockwise for an observer to whom the axis vector is pointing (right hand rule)
+        super().__init__()
         assert (len(group1) > 0) and (len(axis_groups) > 0), 'Cannot operate on empty set'
         assert (len(axis_groups) == 2) or (len(axis_groups) == 3), 'Axis must be defined with 2 or 3 groups'
         for i in np.arange(len(axis_groups)):
@@ -320,7 +327,7 @@ class Transformation_rotationCOM:
             assert (np.max(self.axis_groups) <= len(mol.xyz)), \
                 "Index out of bound: largest index of group 1 > length of supplied molecule"
         self.A_COM = np.sum(mol.xyz[self.axis_groups[0]].T * AtomicMass()[mol.Z_num[self.axis_groups[0]] - 1],
-                                 1) / np.sum(AtomicMass()[mol.Z_num[self.axis_groups[0]] - 1])
+                            1) / np.sum(AtomicMass()[mol.Z_num[self.axis_groups[0]] - 1])
         self.B_COM = np.sum(mol.xyz[self.axis_groups[1]].T * AtomicMass()[mol.Z_num[self.axis_groups[1]] - 1],
                             1) / np.sum(AtomicMass()[mol.Z_num[self.axis_groups[1]] - 1])
         if len(self.axis_groups) == 3:
@@ -333,7 +340,7 @@ class Transformation_rotationCOM:
         if len(self.axis_groups) == 3:  # Use cross product of AB and BC as vector
             self.axis = np.cross(self.B_COM - self.A_COM, self.C_COM - self.B_COM)
 
-        return self
+        # return self
 
     def describe(self):
         if len(self.axis_groups) == 2:
@@ -354,7 +361,10 @@ class Transformation_rotationCOM:
         if amplitude is None:
             amplitude = self.amplitude0
 
-        xyz[self.group1] = rotation3D(xyz[self.group1].T, self.axis, amplitude).T
+        if len(self.axis_groups) == 2:
+            xyz[self.group1] = rotation3D((xyz[self.group1] - self.A_COM).T, self.axis, amplitude).T + self.A_COM
+        if len(self.axis_groups) == 3:
+            xyz[self.group1] = rotation3D((xyz[self.group1] - self.A_COM).T, self.axis, amplitude).T + self.B_COM
 
         return xyz
 